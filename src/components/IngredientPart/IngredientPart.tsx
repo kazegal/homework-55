99f72783bb2b79f22262646910f9f09872359ac7
Rakhmanin Aleksandr <a.rachmanin@yandex.ru>
2024-05-31 23:32:33 +0600

    изменил в компонентах ингредиенты удаление ингредиентов

--- src/components/IngredientPart/IngredientPart.tsx
+++ src/components/IngredientPart/IngredientPart.tsx
@@ -12,18 +12,18 @@ interface IProps {
 }
 
 const IngredientPart: React.FC<IProps> = props => {
-    return (
-        <div className='ingredients-choose'>
-            <img className='ingredients-img'
-                 src={props.image} alt={props.name}
-            />
-            <p className='ingredients-info'>{props.name}({props.price}сом)</p>
-            <button onClick={() => props.addOne(props.name)} className='add-btn'>+</button>
-            <span className='count'>x{props.count}</span>
-            <button onClick={() => props.removeOne(props.name)} className='remove-btn'>-</button>
-            <button onClick={() => props.removeAll(props.name)} className='del-btn'></button>
-        </div>
-    );
+  return (
+      <div className='ingredients-choose'>
+        <img className='ingredients-img'
+             src={props.image} alt={props.name}
+        />
+        <p className='ingredients-info'>{props.name}({props.price}сом)</p>
+        <button onClick={() => props.addOne(props.name)} className='add-btn'>+</button>
+        <span className='count'>x{props.count}</span>
+        <button onClick={() => props.removeOne(props.name)} className='remove-btn'>-</button>
+        {props.count > 0 && <button onClick={() => props.removeAll(props.name)} className='del-btn'></button>}
+      </div>
+  );
 };
 
 export default IngredientPart;
